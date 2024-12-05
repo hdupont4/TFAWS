8335bd529c9ea484245f8458ad26391de13c9215
Hugo <115085341+hdupont4@users.noreply.github.com>
2024-12-05 11:48:30 +0100

    add alb data

--- modules/core-compute/output.tf
+++ modules/core-compute/output.tf
@@ -17,3 +17,15 @@ output "private_subnet_ids" {
     aws_subnet.private_3.id
   ]
 }
+
+output "alb_dns_name" {
+  value = aws_lb.application_lb.dns_name
+}
+
+output "alb_arn" {
+  value = aws_lb.application_lb.arn
+}
+
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
